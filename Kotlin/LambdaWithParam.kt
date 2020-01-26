c933ba3249c1ed898383d73712d84e5b9471b03b
gangjaeum <gangjaeum@gmail.com>
2020-01-26 15:20:37 +0900

    Update LambdaWithParam.kt

--- Kotlin/LambdaWithParam.kt
+++ Kotlin/LambdaWithParam.kt
@@ -1,14 +1,21 @@
 package chap03.section3
 
 fun main() {
-    Param{a ->"HelloWorld! $a"}
+    Param{a ->"Hello $a World!"}
 
-    withArgs("Arg1", "Arg2", {a,b -> "Hello World! $a $b"})
-    withArgs("Arg1", "Arg2") {a,b ->"HelloWorld! $a $b"}
+    withArgs("Happy", "Arg2", {a,b -> "Hello $a World! $b"})
+    withArgs("Happy", "Arg2") {a,b ->"Hello $a World! $b"}
+    
+    twoLambda({a -> "Hello $a World"}, {b -> "Happy $b Smile"})
 }
 
-fun Param(out: (String)->String) = println(out("OneParam"))
+fun Param(out: (String)->String) = println(out("Happy"))
 
 fun withArgs(a: String, b: String, out: (String, String) -> String) {
     println(out(a,b))
+}
+
+fun twoLambda(out1: (String)->String, out2: (String) -> String) {
+    println(out1("Happy"))
+    println(out2("Lucky"))
 }
